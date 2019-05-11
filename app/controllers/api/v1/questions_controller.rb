4c1fe5860a3e32c290ef5599659fe124a93d9dea
vamsi <vamsi@quintype.com>
2019-05-11 14:25:37 +0530

    integration of service with the controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     # Questions controller integrating with the stack exchange service
     class QuestionsController < ApplicationController
-
       def index
         render json: {
           questions: stack_exchange_service.get_questions
@@ -11,8 +10,12 @@ module Api
 
       private
 
+      def query_params
+        params.permit(:sort, :order, :site, :page).to_h
+      end
+
       def stack_exchange_service
-        @stack_exchange_service ||= StackExchange.new
+        @stack_exchange_service ||= StackExchange.new(query_params)
       end
     end
   end
