aa503d81bb6f43f85c5578537b141d4a9a4776d5
vamsi <vamsi@quintype.com>
2019-05-11 14:25:08 +0530

    Service with the get_questions

--- app/services/stack_exchange.rb
+++ app/services/stack_exchange.rb
@@ -1,5 +1,28 @@
+# Service which communicates with stack exchange apis
 class StackExchange
+  def initialize(query_params)
+    @query_params = query_params
+  end
+
   def get_questions
-    "hello"
+    path = 'https://api.stackexchange.com/2.2/questions'
+    send_request(lambda {
+      HTTParty.get(
+        path,
+        options
+      ) # timeout can be read from config
+    }, path)
+  end
+
+  private
+
+  def options
+    { query: @query_params, timeout: 5 }
+  end
+
+  def send_request(request, path)
+    request.call
+  rescue Timeout::Error
+    raise GatewayTimeoutError.new(gateway: 'stack overflow', path: path)
   end
 end
