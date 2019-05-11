1b7ea3675c92cd26c7bdf324eb63ea295dc560cb
vamsi <vamsi@quintype.com>
2019-05-11 16:24:33 +0530

    when it cant find the data in cache

--- spec/questions_controller_spec.rb
+++ spec/questions_controller_spec.rb
@@ -12,13 +12,25 @@ RSpec.describe Api::V1::QuestionsController, type: :controller do
       expect(parsed_response['questions']).to_not eq nil
     end
 
-    it 'reads from cache if service returns bad gateway' do
-      REDIS_CLIENT.set("questions_path{\"sort\"=>\"foo\"}", JSON.dump({"foo": "bar"}))
-      allow_any_instance_of(StackExchange).to receive(:get_questions).and_raise(BadGatewayError)
-      get :index, format: :json, params: { 'sort' => 'foo' }
-      expect(response.status).to eq 200
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['questions']['foo']).to eq 'bar'
+    context 'when service returns bad gateway or timeout' do
+      it 'reads from cache if service returns bad gateway' do
+        REDIS_CLIENT.set("questions_path{\"sort\"=>\"foo\"}", JSON.dump({"foo": "bar"}))
+        allow_any_instance_of(StackExchange).to receive(:get_questions).and_raise(BadGatewayError)
+        get :index, format: :json, params: { 'sort' => 'foo' }
+        expect(response.status).to eq 200
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['questions']['foo']).to eq 'bar'
+      end
+
+      it "when it can't find the cached data" do
+        allow(REDIS_CLIENT).to receive(:get).and_return(nil)
+
+        allow_any_instance_of(StackExchange).to receive(:get_questions).and_raise(BadGatewayError)
+        get :index, format: :json, params: { 'sort' => 'foo' }
+        expect(response.status).to eq 422
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['error']['message']).to eq 'Cannot process the request'
+      end
     end
   end
 end
